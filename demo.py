c842e17d3e5df000de90058bc2b07e935fc1b2d8
Nandan Varma <137528244+NandanUnv@users.noreply.github.com>
2023-09-22 23:16:00 +0530

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,45 @@
+
+print('''              
+ _____ _     _              _         
+|  |  |_|___| |_ ___ ___   | |_ ___   
+|     | | . |   | -_|  _|  |  _| . |  
+|__|__|_|_  |_|_|___|_|    |_| |___|  
+        |___|                                                       
+ __                                   
+|  |   ___ _ _ _ ___ ___              
+|  |__| . | | | | -_|  _|             
+|_____|___|_____|___|_|     
+''')
+
+print('----------------------------------')
+
+data = [
+    {
+        'Name': "Prabhas",
+        'Followers': 2500,
+        'Connections': 150,
+        'Description': 'Movie_Artist',
+        'Country': 'India'
+    },
+    {
+        'Name': "Jay Shankar",
+        'Followers': 200,
+        'Connections': 100,
+        'Description': 'Politician',
+        'Country': 'China'
+    },
+    {
+        'Name': "Rana",
+        'Followers': 2300,
+        'Connections': 900,
+        'Description': 'Movie_Artist',
+        'Country': 'Nepal'
+    },
+    {
+        'Name': "Rajamouli",
+        'Followers': 2600,
+        'Connections': 1100,
+        'Description': 'Director',
+        'Country': 'Bhutan'
+    }
+]
